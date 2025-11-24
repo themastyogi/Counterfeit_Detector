76352fbba6f18d2cff7cf4dcec5a55c60ebd6a00
Vikas Kumar <vikask@inecta.com>
2025-11-24 15:02:51 +0530

    Fix: Update wildcard route syntax for Express 5

--- server/index.js
+++ server/index.js
@@ -109,7 +109,7 @@ app.post('/api/force-admin', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
 }

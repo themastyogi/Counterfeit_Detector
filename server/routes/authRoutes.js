4b35d1cd0a5377f0b37769fa70d749db5a6015f5
Vikas Kumar <vikask@inecta.com>
2025-11-29 11:25:10 +0530

    fix: Use correct verifyToken middleware in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const { verifyToken } = require('../middleware/authMiddleware');
 const { register, login, updatePassword, forgotPassword, resetPassword, fixRole } = require('../controllers/authController');
 
 router.post('/register', register);
@@ -8,6 +8,6 @@ router.post('/login', login);
 router.put('/update-password', updatePassword);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
-router.post('/fix-role', protect, fixRole);
+router.post('/fix-role', verifyToken, fixRole);
 
 module.exports = router;
